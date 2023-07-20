25e9165007305534bc0c794a6a7ea497fb19b8af
Nguyễn Hải Đăng <130114363+Hikaru203@users.noreply.github.com>
2023-07-20 18:54:22 +0700

    [java]source code

---
+++ asmjava6/src/test/java/com/example/asmjava6/Asmjava6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asmjava6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asmjava6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
